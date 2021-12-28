3e86971368ddb849f40f7bde36e759e1635f8c29
lowie <defermelowie@gmail.com>
2021-12-28 10:45:35 +0100

    TLS does not work on free heroku

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gunicorn --certfile=tls/cert.pem --keyfile=tls/key.pem -w 4 --threads 2  --bind 0.0.0.0:443 app:app
+gunicorn -w 4 --threads 2  --bind 0.0.0.0:80 app:app
+#gunicorn --certfile=tls/cert.pem --keyfile=tls/key.pem -w 4 --threads 2  --bind 0.0.0.0:443 app:app

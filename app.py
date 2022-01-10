63a5b19b7a2b178f9e3b29d2c06739c1634ca644
lowie <defermelowie@gmail.com>
2022-01-10 14:47:59 +0100

    Add api kay

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 from datetime import datetime
+
+from flask.json import JSONEncoder
 
 from sun import Sun
 
 app = Flask(__name__, template_folder='./docs')
+
+VALID_API_KEYS = {'B3rDwaXJ24As6Q6o5BEN64VFgemKMRJG',
+                  'DmvvPxZoZSi2SpgmGC4KH2akTeEouk9x'}
 
 
 ###############################################################
@@ -22,17 +27,27 @@ def index():
 
 @app.route('/sunset', methods=['GET'])
 def get_sunset():
-    date = datetime.strptime(request.args.get('date'), '%Y-%m-%d').date()
-    longitude = float(request.args.get('lon'))
-    latitude = float(request.args.get('lat'))
-    sunset = Sun().sunset(longitude, latitude, date)
-    return {'datetime': str(sunset.isoformat('T')), 'location': {'longitude': longitude, 'latitude': latitude}}
+    if (request.args.get('key') in VALID_API_KEYS):
+        date = datetime.strptime(request.args.get('date'), '%Y-%m-%d').date()
+        longitude = float(request.args.get('lon'))
+        latitude = float(request.args.get('lat'))
+        sunset = Sun().sunset(longitude, latitude, date)
+        return {'datetime': str(sunset.isoformat('T')), 'location': {'longitude': longitude, 'latitude': latitude}}
+    else:
+        error = {'error': 'invallid key',
+                 'used-key': str(request.args.get('key'))}
+        return Response(JSONEncoder().encode(error), status=403, mimetype='application/json')
 
 
 @app.route('/sunrise', methods=['GET'])
 def get_sunrise():
-    date = datetime.strptime(request.args.get('date'), '%Y-%m-%d').date()
-    longitude = float(request.args.get('lon'))
-    latitude = float(request.args.get('lat'))
-    sunrise = Sun().sunrise(longitude, latitude, date)
-    return {'datetime': str(sunrise.isoformat('T')), 'location': {'longitude': longitude, 'latitude': latitude}}
+    if (request.args.get('key') in VALID_API_KEYS):
+        date = datetime.strptime(request.args.get('date'), '%Y-%m-%d').date()
+        longitude = float(request.args.get('lon'))
+        latitude = float(request.args.get('lat'))
+        sunrise = Sun().sunrise(longitude, latitude, date)
+        return {'datetime': str(sunrise.isoformat('T')), 'location': {'longitude': longitude, 'latitude': latitude}}
+    else:
+        error = {'error': 'invallid key',
+                 'used-key': str(request.args.get('key'))}
+        return Response(JSONEncoder().encode(error), status=403, mimetype='application/json')
